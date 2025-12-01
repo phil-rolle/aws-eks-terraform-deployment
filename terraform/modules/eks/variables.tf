687e1f4e312029ccd9aaaccbb71dcf1662864f1d
Phil Rolle <phil.rolle@ada.support>
2025-12-01 11:51:27 -0500

    Update Kubernetes version to 1.31

--- terraform/modules/eks/variables.tf
+++ terraform/modules/eks/variables.tf
@@ -9,7 +9,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.31"
 }
 
 variable "cluster_role_arn" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -22,7 +22,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version for the EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.31"
 }
 
 variable "vpc_cidr" {
